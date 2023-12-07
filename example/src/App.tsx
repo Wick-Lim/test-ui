7e36deb534ab6b86f56a65ed80acb45db0e76406
임준혁 <wick@rsquare.co.kr>
2023-12-07 15:20:50 +0900

    base commit

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'test-ui'
+import 'test-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
