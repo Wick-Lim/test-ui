cf93fa2f72c6897a6bbee7dbd93e7c6c45babd4f
임준혁 <wick@rsquare.co.kr>
2023-12-14 16:53:24 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,41 @@ import loadable from "@loadable/component";
 import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { Link, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
+import Logo from "./assets/rsq-logo-large.svg";
+
+const XProperty = loadable.lib(
+  // @ts-ignore
+  () => import("https://test-menu-1.vercel.app/service.js")
+);
+const XCollect = loadable.lib(
+  // @ts-ignore
+  () => import("https://test-menu-1.vercel.app/service.js")
+);
+
 const XInbound = loadable.lib(
   // @ts-ignore
-  () => import("https://test-menu-1.vercel.app/service.x-inbound.js")
+  () => import("https://test-menu-1.vercel.app/service.js")
 );
 const XClientInform = loadable.lib(
   // @ts-ignore
-  () => import("https://test-menu-1.vercel.app/service.x-inbound.js")
+  () => import("https://test-menu-1.vercel.app/service.js")
 );
 const XMarketing = loadable.lib(
   // @ts-ignore
-  () => import("https://test-menu-1.vercel.app/service.x-inbound.js")
+  () => import("https://test-menu-1.vercel.app/service.js")
 );
 const XSales = loadable.lib(
   // @ts-ignore
-  () => import("https://test-menu-1.vercel.app/service.x-inbound.js")
+  () => import("https://test-menu-1.vercel.app/service.js")
 );
 const XFollowUp = loadable.lib(
   // @ts-ignore
-  () => import("https://test-menu-1.vercel.app/service.x-inbound.js")
+  () => import("https://test-menu-1.vercel.app/service.js")
+);
+
+const XSetting = loadable.lib(
+  // @ts-ignore
+  () => import("https://test-menu-1.vercel.app/service.js")
 );
 
 export default function App() {
@@ -33,17 +49,24 @@ export default function App() {
     <Box display="flex" flexDirection="column" flex="1">
       <AppBar position="sticky" color="default" elevation={1}>
         <Toolbar>
-          <Typography variant="h6" component={Link} to="/">RSQUARE PORTAL</Typography>
+          <img src={Logo} alt="logo" width="auto" height="16px" onClick={() => {
+            navigate('/');
+          }} style={{
+            cursor: 'pointer'
+          }} />
 
           <Box width="32px" />
 
           <Tabs value={tab} onChange={(_, v) => navigate(`/${v}`)}>
             <Tab value="" sx={{ minWidth: 0, width: 0, p: 0 }} />
+            <Tab value="property" label="건물/매물" />
+            <Tab value="collect" label="정보수집/지원" />
             <Tab value="inbound" label="인바운드" />
             <Tab value="client-inform" label="고객정보관리" />
             <Tab value="marketing" label="마케팅" />
             <Tab value="sales" label="영업관리" />
             <Tab value="follow-up" label="사후관리" />
+            <Tab value="setting" label="설정" />
           </Tabs>
         </Toolbar>
       </AppBar>
@@ -51,6 +74,16 @@ export default function App() {
       <Box flex="1" position="relative">
         <Routes>
           <Route path="/" element={<div>Home</div>} />
+          <Route
+            path="/property/*"
+            // @ts-ignore
+            element={<XProperty children={() => <x-property />} />}
+          />
+          <Route
+            path="/collect/*"
+            // @ts-ignore
+            element={<XCollect children={() => <x-collect />} />}
+          />
           <Route
             path="/inbound/*"
             // @ts-ignore
@@ -75,6 +108,11 @@ export default function App() {
             path="/follow-up/*"
             // @ts-ignore
             element={<XFollowUp children={() => <x-follow-up />} />}
+          />
+          <Route
+            path="/setting/*"
+            // @ts-ignore
+            element={<XSetting children={() => <x-setting />} />}
           />
         </Routes>
       </Box>
